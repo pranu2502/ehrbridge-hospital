00f74716f9170391565858c735fafa766e9a05d3
pranu2502 <spranavreddy25@gmail.com>
2023-04-30 00:19:45 +0530

    Changed Get consent by patient if

--- src/main/java/com/ehrbridge/hospital/repository/PatientRecordsRepository.java
+++ src/main/java/com/ehrbridge/hospital/repository/PatientRecordsRepository.java
@@ -1,10 +1,15 @@
 package com.ehrbridge.hospital.repository;
 
+import com.ehrbridge.hospital.entity.Patient;
 import com.ehrbridge.hospital.entity.PatientRecords;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.swing.*;
+import java.util.List;
+import java.util.Optional;
 
 public interface PatientRecordsRepository extends JpaRepository<PatientRecords, String> {
+
+    Optional<List<PatientRecords>> findPatientRecordsByPatientID(String patientId);
     
 }
